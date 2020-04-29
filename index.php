a51a836f45bae0b21ee75ba75045d5f0f01c40aa
edisonzamora <37243420+edisonzamora@users.noreply.github.com>
2020-04-29 08:57:28 +0200

    Revert "[edisonzamora]dev"
    This reverts commit 31b9e1811314218abe56d6681509ef34a8d0ecf0.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $host  =     $_SERVER["HTTP_HOST"];
 $path  =     $http."://".$host."/Rexpress/";
 $url_style=  $path."view/style";
 $url_js=     $path."view/js";
-//cometario de nuevo para admin
+//cometario nuevo Hola admin
 ?>
     <head>
         <meta charset="UTF-8">
